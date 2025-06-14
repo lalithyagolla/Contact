4142207984a6fa12e406ca63bf87b8788bf431e0
Lalithya Golla <108351773+lalithyagolla@users.noreply.github.com>
2025-06-14 10:08:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,8 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
 app.use('/api', contactRoutes);
 
 mongoose.connect(process.env.MONGO_URI, {
